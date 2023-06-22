3e067cb1451ea467d13d01ba355c2aec5a0c61ac
hamzahmiza175 <hamzahmiza175@gmail.com>
2023-06-22 13:30:34 +0100

    Add App Icons

--- lib/app/core/utils/app_icons_keys.dart
+++ lib/app/core/utils/app_icons_keys.dart
@@ -5,4 +5,21 @@
 
 class AppIconsKeys {
   AppIconsKeys._();
+
+  static const String _path = "assets/icons";
+  static const String books = "$_path/books.svg";
+  static const String arrowBottom = "$_path/arrow_bottom.svg";
+  static const String edit = "$_path/edit.svg";
+  static const String facebook = "$_path/facebook.svg";
+  static const String file = "$_path/file.svg";
+  static const String happyFace = "$_path/happy_face.svg";
+  static const String idea = "$_path/idea.svg";
+  static const String instagram = "$_path/instagram.svg";
+  static const String recent = "$_path/recent.svg";
+  static const String search = "$_path/search.svg";
+  static const String setting = "$_path/setting.svg";
+  static const String share_circle = "$_path/share_circle.svg";
+  static const String whatsapp = "$_path/whatsapp.svg";
+  static const String world = "$_path/world.svg";
+  // static const String x = "$_path/.svg";
 }
